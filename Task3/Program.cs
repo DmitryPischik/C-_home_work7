0018046fc942e96d0ad7bd246afc235a8b93a4ff
Dmitriy Pischik <pischik@bk.ru>
2023-02-05 14:21:04 +1000

    MinSomOfLine function is added

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// В прямоугольной матрице найти строку с наименьшей суммой элементов.
+
+void MinSumOfLine(int[,] array) // функция , которая в прямоугольной матрице находит строку с наименьшей суммой элементо.
+{
+    int sum = 0;
+    int minSum = 0;
+    int minImdex = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (i == 0)
+        {
+            minSum = sum;
+        }
+        else if (sum < minSum)
+        {
+            minSum = sum;
+            minImdex = i;
+        }
+        sum = 0;
+    }
+    Console.WriteLine($"Строка с индексом {minImdex} имеет наименьшую сумму элементов - {minSum}");
+}
+void PrintArray(int[,] matr) // функция просматривает массив.
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}   ");
+        }
+    Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr) // функция заполняет массив.
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(0, 10);
+        }
+    }
+}
+///////////////////////////////////
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int[,] array = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
+MinSumOfLine(array);
