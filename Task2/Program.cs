8d1b3470fc5d519befab4bcafdf45a9f10203b23
Dmitriy Pischik <pischik@bk.ru>
2023-02-05 13:26:39 +1000

    Folder Task2 i added

---
+++ Task2/Program.cs
@@ -0,0 +1,54 @@
+﻿// Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива.
+
+void SortToLower(int[,] array) // функция упорядочивания по убыванию элементов каждой строки двумерной массива.
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int x = 0; x < array.GetLength(1) - 1; x++)
+            {
+                if (array[i, x] < array[i, x + 1])
+                {
+                    int temp = array[i, x + 1];
+                    array[i, x + 1] = array[i, x];
+                    array[i, x] = temp;
+                }
+            }
+        }
+    }
+}
+void PrintArray(int[,] matr) // функция просматривает массив.
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}   ");
+        }
+    Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr) // функция заполняет массив.
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(0, 10);
+        }
+    }
+}
+///////////////////////////////////
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int[,] array = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
+SortToLower(array);
+Console.WriteLine();
+PrintArray(array);
