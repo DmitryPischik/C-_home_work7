3253ec715817640306f7b7523dc4b3038c48ed4d
Dmitriy Pischik <pischik@bk.ru>
2023-02-05 13:13:01 +1000

    Folder Task1 is added

---
+++ Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Написать программу, которая обменивает элементы первой строки и последней строки
+
+void PrintArray(int[,] matr) // функция просматривает массив.
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}   ");
+        }
+    Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr) // функция заполняет массив.
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int[,] array = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
+// Обмениваеv элементы первой строки и последней строки:
+for(int j=0; j < array.GetLength(1); j++)
+{
+    int temp = array[0, j];
+    array[0, j] = array[m - 1, j];
+    array[m - 1, j] = temp;          
+}
+Console.WriteLine();
+PrintArray(array);
